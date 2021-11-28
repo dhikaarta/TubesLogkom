8a927f6cdb419b0e3520a881a8034405a74a4474
dhikaarta <16520019@std.stei.itb.ac.id>
2021-11-28 20:14:07 +0700

    added new features

--- weather.pl
+++ weather.pl
@@ -5,18 +5,19 @@ currentWeather(sunny).
 weather(rainy).
 weather(sunny).
 weather(snowing).
-weather(flood).
-weather(hailstorm).
 
 isValidWeather(X) :- weather(X).
 
-weatherRandomizer :- \+ currentSeason(winter), random(1,101, X),
-                     (X < 3 -> retract(currentWeather(_)), assertz(currentWeather(flood)), write('Its flooding today ! You cant go out there, just go back to sleep ! '),nl,sleep;
-                      X < 51 -> retract(currentWeather(_)), assertz(currentWeather(rainy)), write('Its raining today ! Be careful out there.'), nl;
+weatherRandomizer :- \+ currentSeason(winter),\+ currentSeason(summer), random(1,101, X),
+                     (X < 51 -> retract(currentWeather(_)), assertz(currentWeather(rainy)), write('Its raining today ! Be careful out there.'), nl;
                       X > 50 -> retract(currentWeather(_)), assertz(currentWeather(sunny)), write('What a beatiful day ! The sun is shining brightly today !'), nl), !.
 
+weatherRandomizer :- currentSeason(summer), random(1,101, X),
+                     (X < 31 -> retract(currentWeather(_)), assertz(currentWeather(rainy)), write('Its raining today ! Be careful out there.'), nl;
+                      X > 30 -> retract(currentWeather(_)), assertz(currentWeather(sunny)), write('What a beatiful day ! The sun is shining brightly today !'), nl), !.
+
 weatherRandomizer :-  currentSeason(winter), random(1,101, X),
-                      (X < 3 -> retract(currentWeather(_)), assertz(currentWeather(hailstorm)), write('Theres a hailstorm !!! You cant go out, you should just rest inside'), nl,sleep;
+                      (X < 20 ->retract(currentWeather(_)), assertz(currentWeather(sunny)), write('What a beatiful day ! The sun is shining brightly today !'), nl;
                        retract(currentWeather(_)), assertz(currentWeather(snowing)), write('Its snowing! '), nl),!.
 
 changeWeather(X) :-
